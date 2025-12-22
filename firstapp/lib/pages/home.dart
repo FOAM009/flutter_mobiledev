cb221d127cbb00cad656f7945e69f93dd099d680
FOAM <Chonticha_kumma@cmu.ac.th>
2025-12-22 17:49:10 +0700

    finish 7.1 different image MyBox &&  read more>>

--- firstapp/lib/pages/home.dart
+++ firstapp/lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:firstapp/pages/detail.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -13,17 +14,29 @@ class _HomepageState extends State<Homepage> {
     return ListView(
       padding: const EdgeInsets.all(20),
       children: [
-        MyBox(),
+        MyBox(
+          "What is a computer?",
+          "Computer is a things to calculate and fo any other works",
+          "https://media1.tenor.com/m/b0cev0cRVSYAAAAC/yes-chef.gif",
+        ),
         SizedBox(height: 20),
-        MyBox(),
+        MyBox(
+          "What is a Flutter?",
+          "Flutter is a tool to create a mobile application",
+          "https://media1.tenor.com/m/vYrfrjSAI4QAAAAd/chef-muppets.gif",
+        ),
         SizedBox(height: 20),
-        MyBox(),
+        MyBox(
+          "What is a Dart?",
+          "Dart is the language used in Flutter",
+          "https://media1.tenor.com/m/jG-m1kDAe5oAAAAd/cooking-cook.gif",
+        ),
         SizedBox(height: 20),
       ],
     );
   }
 
-  Widget MyBox() {
+  Widget MyBox(String title, String subtitle, String img_url) {
     return Container(
       padding: EdgeInsets.all(20),
       height: 150,
@@ -31,9 +44,7 @@ class _HomepageState extends State<Homepage> {
         color: Colors.blue[200],
         borderRadius: BorderRadius.circular(20),
         image: DecorationImage(
-          image: NetworkImage(
-            "https://cdn.pixabay.com/photo/2023/11/18/18/20/christmas-8396941_1280.png",
-          ),
+          image: NetworkImage(img_url),
           fit: BoxFit.cover,
           colorFilter: ColorFilter.mode(
             Colors.black.withOpacity(0.5),
@@ -46,7 +57,7 @@ class _HomepageState extends State<Homepage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "What is a computer",
+            title,
             style: TextStyle(
               fontSize: 25,
               color: Colors.white,
@@ -54,8 +65,19 @@ class _HomepageState extends State<Homepage> {
             ),
           ),
           Text(
-            "Computer is a things to calculate and fo any other works",
-            style: TextStyle(fontSize: 15, color: Colors.white),
+            subtitle,
+            style: TextStyle(fontSize: 15, color: Colors.blueGrey),
+          ),
+          SizedBox(height: 15),
+          TextButton(
+            onPressed: () {
+              print("next page >>");
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailPage()),
+              );
+            },
+            child: Text("read more", style: TextStyle(color: Colors.amber)),
           ),
         ],
       ),
